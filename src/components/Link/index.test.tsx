82e7933a9077902671de494eeef4bcd49300d73d
UcheSylvester <okorocode@gmail.com>
2022-04-23 09:48:21 +0100

    test link

---
+++ src/components/Link/index.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+
+import Link, { LinkProps } from ".";
+
+const renderLink = (props?: Partial<LinkProps>) => {
+  const defaultProps: LinkProps = {
+    children: "home",
+    to: "/",
+  };
+
+  render(
+    <MemoryRouter>
+      <Link {...defaultProps} {...props} />
+    </MemoryRouter>
+  );
+};
+
+describe("<Link />", () => {
+  it("should render children", () => {
+    renderLink();
+
+    const link = screen.getByRole("link", { name: "home" });
+    expect(link).toBeInTheDocument();
+  });
+
+  it("should render to", () => {
+    renderLink();
+
+    const link = screen.getByRole("link", { name: "home" });
+    expect(link).toHaveAttribute("href", "/");
+  });
+
+  it("should render underlined class", () => {
+    renderLink({ isUnderlined: true });
+
+    const link = screen.getByRole("link", { name: "home" });
+    expect(link).toHaveClass("link--underlined");
+  });
+});
